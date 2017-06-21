bbebd537354aaa9e92f4193bda4760d5e87227f1
AlinaBarsukevich <fedakina@mail.ru>
2017-06-21 11:57:16 +0300

    Dev1

---
+++ Dev1.java
@@ -0,0 +1,13 @@
+public class Dev1 {
+    public static void main (String[] args) {
+
+        for (int i=0;i<=100; i++) {
+            if(i % 3 == 0) {
+        System.out.print("3*" + i/3);
+          } else {
+        System.out.print(i);
+
+          }
+       }
+    }
+ }
