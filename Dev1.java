08bf7210f94d4745c7c934fff3c0b8027d957c0f
AlinaBarsukevich <fedakina@mail.ru>
2017-06-21 14:09:02 +0300

    Update Dev1.java

--- Dev1.java
+++ Dev1.java
@@ -1,11 +1,10 @@
 public class Dev1 {
   public static void main (String[] args) {
-    for (int i = 0;i <= 100; i++) {
+    for (int i = 0; i <= 100; i++) {
       if (i % 3 == 0) {
         System.out.println("3*" + i/3);
       } else {
         System.out.println(i);
-
       }
     }
   }
