045d8c1a708865d2d1291bb56da4b642f5fd633a
AlinaBarsukevich <fedakina@mail.ru>
2017-06-22 13:11:15 +0300

    Update Dev2.java

--- Dev2.java
+++ Dev2.java
@@ -2,13 +2,13 @@ public class Dev2 {
   public static void main (String[] args) {
     for (int i=0;i<=100; i++) {
       if (i % 3 == 0 && i % 5 == 0) {
-        System.out.print("tutti-frutti");
+        System.out.println("tutti-frutti");
       } else if (i % 5 == 0) {
-        System.out.print("frutti");
+        System.out.println("frutti");
       } else if (i % 3 == 0) {
-        System.out.print("tutti");
+        System.out.println("tutti");
       } else {
-        System.out.print(i);
+        System.out.println(i);
 
 
       }
