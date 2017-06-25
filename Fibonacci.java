90feb4306376acb09b54b044b7a87c3e42ef1d6b
AlinaBarsukevich <fedakina@mail.ru>
2017-06-25 11:54:28 +0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -8,12 +8,12 @@ public class Fibonacci {
     Scanner reader = new Scanner(System.in);
     System.out.println("Enter number : ");
     int enteredNumber = reader.nextInt();
-      if (enteredNumber < 0) {
+    if (enteredNumber < 0) {
         System.out.println("This is negative number");
         System.exit(0);
       } else {     
         for (n = 0; enteredNumber > numberFib; n++) {
-          numberFib = Math.round(Math.pow(phi, n) / Math.sqrt(5));
+        numberFib = Math.round(Math.pow(phi, n) / Math.sqrt(5));
 	  if (enteredNumber == numberFib) {
 	    System.out.println("Entered number is one of the Fibonacci's numbers");
           } else {
