a46d0d5fa3299193c053af97787e235b19cc68d9
AlinaBarsukevich <fedakina@mail.ru>
2017-06-29 11:00:18 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 
 public class Sequence {
   public static void main(String[] args) {
-  System.out.println("Enter the numbers through a space: ");
-  Scanner in = new Scanner(System.in);
-  String seq = in.nextLine();
-  String seqParts[] = seq.split(" ");
-  int length = seqParts.length;
-  try {
-    for (int i = 0; i < length - 1; i++) {
-      if (Integer.parseInt(seqParts[i + 1]) > Integer.parseInt(seqParts[i])) {
-        System.out.println("The numbers aren't decreasing");
-        return;
-      }
-    } 
-  } catch (NumberFormatException ex) {
-    System.out.println("It's not available format.");
-    return;
-  }
+    System.out.println("Enter the numbers through a space: ");
+    Scanner in = new Scanner(System.in);
+    String seq = in.nextLine();
+    String seqParts[] = seq.split(" ");
+    int length = seqParts.length;
+    try {
+      for (int i = 0; i < length - 1; i++) {
+        if (Integer.parseInt(seqParts[i + 1]) > Integer.parseInt(seqParts[i])) {
+          System.out.println("The numbers aren't decreasing");
+          return;
+        }
+      } 
+    } catch (NumberFormatException ex) {
+      System.out.println("It's not available format.");
+      return;
+    }
     System.out.println("The numbers are decreasing");
   }
 }
